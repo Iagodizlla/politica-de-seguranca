e1533f64698b60e96d9a4b9881ebefa6a353d9f2
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2026-03-21 23:25:25 -0300

    Adicionando os documentos aos links de acesso

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const documentos = [
-    { titulo: "📄 Política Geral", desc: "Diretrizes principais", link: "normas/politica_geral.html" },
-    { titulo: "🧾 Termo de Uso", desc: "Regras do sistema", link: "normas/termo_uso.html" },
-    { titulo: "🌐 Acesso Remoto", desc: "Uso fora da empresa", link: "normas/acesso_remoto.html" },
-    { titulo: "💻 Equipamentos", desc: "Dispositivos pessoais", link: "normas/equipamentos.html" },
-    { titulo: "🔎 Classificação", desc: "Proteção de dados", link: "normas/classificacao.html" },
-    { titulo: "🛡️ Malware", desc: "Segurança digital", link: "normas/malware.html" },
-    { titulo: "📧 E-mail", desc: "Comunicação segura", link: "normas/email.html" },
-    { titulo: "🔑 Acesso", desc: "Controle de usuários", link: "normas/acesso.html" },
-    { titulo: "🌍 Internet", desc: "Uso da internet", link: "normas/internet.html" },
-    { titulo: "📊 Monitoramento", desc: "Auditoria", link: "normas/monitoramento.html" },
-    { titulo: "🚨 Incidentes", desc: "Resposta a falhas", link: "normas/incidentes.html" },
-    { titulo: "✔ Uso Aceitável", desc: "Boas práticas", link: "normas/uso_aceitavel.html" }
+    { titulo: "📄 Política Geral", desc: "Diretrizes principais", link: "normas/033 SOLUS-PGSI-R1.pdf" },
+    { titulo: "🧾 Termo de Uso", desc: "Regras do sistema", link: "normas/033 SOLUS-TUSI-R1_Termos_Uso.pdf" },
+    { titulo: "🌐 Acesso Remoto", desc: "Uso fora da empresa", link: "normas/033 SOLUS-Acesso-Remoto-R1.pdf" },
+    { titulo: "💻 Equipamentos", desc: "Dispositivos pessoais", link: "normas/033 SOLUS-BYOD-R1.pdf"},
+    { titulo: "🔎 Classificação", desc: "Proteção de dados", link: "normas/033 SOLUS-Classificacao-Informacao-R1.pdf" },
+    { titulo: "🛡️ Malware", desc: "Segurança digital", link: "normas/033 SOLUS-Cod-Maliciosos-R1.pdf" },
+    { titulo: "📧 E-mail", desc: "Comunicação segura", link: "normas/033 SOLUS-E-MAIL-e-IM-R1.pdf" },
+    { titulo: "🔑 Acesso", desc: "Controle de usuários", link: "normas/033 SOLUS-Gestao-Identidade-R1.pdf"},
+    { titulo: "🌍 Internet", desc: "Uso da internet", link: "normas/033 SOLUS-Internet-e-Midias-Sociais-R1.pdf" },
+    { titulo: "📊 Monitoramento", desc: "Auditoria", link: "normas/033 SOLUS-Monitoramento-R1.pdf" },
+    { titulo: "🚨 Incidentes", desc: "Resposta a falhas", link: "normas/033 SOLUS-Resposta-a-incidente-R1.pdf" },
+    { titulo: "✔ Uso Aceitável", desc: "Boas práticas", link: "normas/033 SOLUS-Uso-Aceitavel-Ativos-Informacao-R1.pdf" }
 ];
 
 const container = document.getElementById("cards-container");
@@ -22,7 +22,7 @@ documentos.forEach((doc, index) => {
     card.innerHTML = `
         <h3>${doc.titulo}</h3>
         <p>${doc.desc}</p>
-        <a class="btn" href="${doc.link}">Abrir</a>
+        <a class="btn" href="${doc.link}" target="_blank">Abrir</a>
     `;
 
     container.appendChild(card);
